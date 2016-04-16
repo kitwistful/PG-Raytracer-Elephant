bc3fc9f54c24c8266af435e638fbcbc79675cd39
Kathryn McKay <kathryn.mckay@uoit.net>
2016-04-16 16:00:49 -0400

    Implemented partitions

--- scratch/quadtreedemo.hs
+++ scratch/quadtreedemo.hs
@@ -1,10 +1,19 @@
 import QuadTree
 
-quadTree1 = QuadTreeNode { size = (10.0, 10.0), objects = [],
-  children = (QuadTreeTip, QuadTreeTip, QuadTreeTip, QuadTreeTip),
-  depth = 0 }
+import Data.FixedList
+
+quadTree1 = quadTreeRoot 1.0 0.0 19.0 10.0
 
 main = do
-  putStrLn " I think it's working"
   putStrLn $ show quadTree1
   putStrLn $ "height: " ++ show (quadTreeHeight quadTree1)
+  putStrLn $ concat (map (\x -> show x 
+                              ++ " " 
+                              ++ show (quadrantRange (areaCovered quadTree1) x)
+                              ++ "\n")
+                      [(FirstQuadrant)..(FourthQuadrant)])
+  putStrLn $ concat (map (\x -> show x
+                             ++ " falls in "
+                             ++ show (pointFallsIntoQuadrant (areaCovered quadTree1) x)
+                             ++ "\n")
+                      [[0.0, 0.0], [9.5, 1.0], [1.0, 5.0], [9.5, 5.0], [9.0, 9.9], [4.9, 0.0]])
